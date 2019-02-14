a5b17c03644ac5eaa373285b17f64fd919431bec
Natalia Vega <nvegar@gmail.com>
2019-02-14 16:17:32 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :cocktails, only: [:index, :show, :create, :new] do
+    resources :doses, only: [:show, :new, :create, :destroy]
+  end
 end
